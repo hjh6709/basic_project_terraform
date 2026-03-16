39b3f5ecf14b9b14ef8ca866d519119df65c10c4
hjh6709 <jh6780h@naver.com>
2026-03-16 14:40:24 +0900

    terraform/environments/aws-stanby/version.tf backend s3 설정 추가

--- terraform/environments/aws-standby/versions.tf
+++ terraform/environments/aws-standby/versions.tf
@@ -7,4 +7,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "hybrid-project-terraform-state"
+    key            = "aws-standby/terraform.tfstate"
+    region         = "ap-northeast-2"
+    dynamodb_table = "hybrid-project-terraform-lock"
+    encrypt        = true
+  }
 }
