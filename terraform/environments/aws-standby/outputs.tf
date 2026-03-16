14b46d80478292dee4c9b0a05647dc39ae47927c
hjh6709 <jh6780h@naver.com>
2026-03-16 21:32:07 +0900

    ansible 폴더 추가 및 workflows ansible 추가

--- terraform/environments/aws-standby/outputs.tf
+++ terraform/environments/aws-standby/outputs.tf
@@ -17,5 +17,27 @@ output "instance_id" {
 
 output "ssh_command" {
   description = "EC2 SSH 접속 명령어"
-  value       = "ssh -i ~/.ssh/키페어파일명.pem ubuntu@${module.compute.ec2_elastic_ip}"
+  value       = "ssh -i ~/.ssh/chilseong-jh.pem ubuntu@${module.compute.ec2_elastic_ip}"
+}
+
+# ansible inventory 자동 생성
+resource "local_file" "ansible_inventory" {
+  content = yamlencode({
+    all = {
+      children = {
+        aws_standby = {
+          hosts = {
+            k3s_node = {
+              ansible_host                 = module.compute.ec2_elastic_ip
+              ansible_user                 = "ubuntu"
+              ansible_ssh_private_key_file = "~/.ssh/chilseong-jh.pem"
+              ansible_ssh_common_args      = "-o StrictHostKeyChecking=no"
+            }
+          }
+        }
+      }
+    }
+  })
+  filename        = "${path.module}/../../ansible/inventory/hosts.yml"
+  file_permission = "0644"
 }
